3fc246bf0fd74207114fc1e32a16de16bc04fc17
Zepher Ashe <safesploit@users.noreply.github.com>
2023-11-23 20:36:33 +0000

    updates

--- quick-memory-buttons/README.md
+++ quick-memory-buttons/README.md
@@ -8,19 +8,23 @@ This script adds quick memory buttons to your Proxmox Virtual Machine (VM) Wizar
 
 ## Installation
 
-1. Make sure you have a user script manager extension installed in your browser. You can use extensions like Tampermonkey or Greasemonkey.
+1. Make sure you have a user script manager extension installed in your browser. You can use extensions like [Tampermonkey](https://www.tampermonkey.net/) or Greasemonkey.
 
 2. Download the [quick-memory-buttons.user.js](quick-memory-buttons.user.js) script file from this repository.
 
-3. Open the script file using your user script manager extension, and it should prompt you to install the script.
+3. As necessary edit the `// @match <domain names>`. I currently have four `@match` domain names listed.
 
-4. After installation, the script will be active on your Proxmox Virtual Environment (VE) web interface.
+4. Open the script file using your user script manager extension, and it should prompt you to install the script.
+   
+    Alternatively, in Tampermonkey press 'create new script..' and paste [quick-memory-buttons.user.js](quick-memory-buttons.user.js).
+
+5. After installation, the script will be active on your Proxmox Virtual Environment (VE) web interface.
 
 ## Usage
 
 Once the script is installed, visit your Proxmox VE web interface. When you open the VM Wizard to create or edit a virtual machine, you should see additional quick memory buttons that allow you to easily set memory sizes for common configurations.
 
-## Customization
+## Customisation
 
 If you need to customize the script for your specific Proxmox setup, make sure to update the `@match` metadata at the beginning of the script to match your Proxmox URL. This ensures that the script is applied correctly to your Proxmox VE instance.
 
---
+++ quick-memory-buttons/quick-memory-buttons.user.js
@@ -0,0 +1,108 @@
+// ==UserScript==
+// @name            Proxmox Quick CT/VM Memory Buttons
+// @namespace       http://tampermonkey.net/
+// @version         1.1
+// @description     Add memory size buttons to the Create CT/VM Wizard
+// @author          safesploit
+// @initial author  _Landmine_ from Reddit
+// @match           https://pve1:8006
+// @match           https://pve2:8006
+// @match           https://pve3:8006
+// @match           https://pve4:8006
+// @grant           none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Converts GB to MiB
+    function gbToMiB(gb) {
+        return gb * 1024;
+    }
+
+    // Creates a button element
+    function createButton(size) {
+        const button = document.createElement('button');
+        button.textContent = `${size}GB`;
+        button.style.marginRight = '5px';
+        button.onclick = function() {
+            const inputField = document.querySelector('input[name="memory"]');
+            if (inputField) {
+                inputField.focus();
+                inputField.value = gbToMiB(size);
+                inputField.dispatchEvent(new Event('input', { bubbles: true }));
+                inputField.dispatchEvent(new Event('change', { bubbles: true }));
+                inputField.blur();
+            }
+        };
+
+        return button;
+    }
+
+    // Adds buttons to the panel
+    function addButtons(panelBody) {
+        const memorySizes = [1, 2, 4, 8, 16, 32, 64];
+        const buttonContainer = document.createElement('div');
+        buttonContainer.id = 'custom-button-container';
+        buttonContainer.style.display = 'flex';
+        buttonContainer.style.flexDirection = 'row';
+        buttonContainer.style.alignItems = 'center';
+        buttonContainer.style.justifyContent = 'flex-start';
+
+        memorySizes.forEach(size => {
+            buttonContainer.appendChild(createButton(size));
+        });
+
+        panelBody.appendChild(buttonContainer);
+    }
+
+    // Main function to find the memory input and place buttons next to it
+    function placeButtons() {
+        const labels = document.querySelectorAll('label.x-form-item-label');
+        const memoryLabel = Array.from(labels).find(label => label.textContent.includes('Memory (MiB):'));
+
+        if (memoryLabel) {
+            const inputFieldContainer = memoryLabel.closest('.x-form-item');
+            const panel = inputFieldContainer.closest('.x-panel');
+
+            let panelBody = panel.nextElementSibling.querySelector('.x-panel-body');
+            if (!panelBody) {
+                panelBody = document.createElement('div');
+                panelBody.classList.add('x-panel-body', 'x-panel-body-default');
+                panel.nextElementSibling.appendChild(panelBody);
+            }
+
+            panel.style.height = 'auto';
+            panel.nextElementSibling.style.height = 'auto';
+            panelBody.style.height = 'auto';
+
+            if (!panelBody.querySelector('#custom-button-container')) {
+                addButtons(panelBody);
+            }
+        }
+    }
+
+    // Use a MutationObserver to listen for changes in the DOM
+    const observer = new MutationObserver(mutations => {
+        let shouldPlaceButtons = false;
+
+        for (const mutation of mutations) {
+            if (mutation.addedNodes.length || mutation.type === 'attributes') {
+                shouldPlaceButtons = true;
+            }
+        }
+
+        if (shouldPlaceButtons) {
+            placeButtons();
+        }
+    });
+
+    observer.observe(document.body, {
+        childList: true,
+        attributes: true,
+        subtree: true,
+        attributeFilter: ['style', 'class']
+    });
+
+    placeButtons();
+})();
